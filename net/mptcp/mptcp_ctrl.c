1fc42b18ad23bc4a51a140db894577e5f52df668
David Heidelberger <david.heidelberger@ixit.cz>
2014-07-25 23:59:59 +0200

    New 3.12 maintenance release
    Signed-off-by: David Heidelberger <david.heidelberger@ixit.cz>

--- net/mptcp/mptcp_ctrl.c
+++ net/mptcp/mptcp_ctrl.c
@@ -2270,7 +2270,7 @@ void __init mptcp_init(void)
 	if (mptcp_register_path_manager(&mptcp_pm_default))
 		goto register_pm_failed;
 
-	pr_info("MPTCP: release for 3.12 series, v0.88.11.1");
+	pr_info("MPTCP: release for 3.12 series, v0.88.12.0");
 
 	mptcp_init_failed = false;
 
